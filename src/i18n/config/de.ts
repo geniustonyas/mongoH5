6985456418a393fec91150c78ec092e3d278bed6
siliez <siliez@qq.com>
2023-07-22 16:52:05 +0800

    调整语言

--- src/i18n/config/de.ts
+++ src/i18n/config/de.ts
@@ -309,361 +309,92 @@ export default {
     vipContent: `
       <div class="vip-clubhouse-box">
         <div class="vc-rows">
-          <div class="vr-title">一、VIP俱乐部介绍</div>
+          <div class="vr-title">1、VIP Club Introduction</div>
           <div class="vr-conts">
-            <p>欢迎来到Seabet尊贵会员俱乐部</p>
-            <p>在Seabet，我们致力于提升游戏品质，并提供最佳的游戏体验。我们推出了一个全新的尊贵会员俱乐部，以奖励在我们网站花费时间游戏并忠实于我们网站的会员朋友。</p>
-            <p>只需投注您所喜爱的游戏/体育赛事等任意一款由Seabet.io提供的游戏服务并通过等级的上升即可解锁独家奖励。</p>
+            <p>Welcome to the Seabet VIP Club!</p>
+            <p>At Seabet, we are committed to enhancing the quality of gaming and providing the best gaming experience. We are excited to introduce our brand new VIP Club, designed to reward our loyal members who spend time playing games on our website.</p>
+            <p>Simply place bets on any game or sports event provided by Seabet.io to unlock exclusive rewards as you climb through the ranks.</p>
           </div>
         </div>
         <div class="vc-rows">
-          <div class="vr-title">二、个性化服务</div>
+          <div class="vr-title">2、Personalized Service</div>
           <div class="vr-conts">
-            <p>专业的客户经理团队确保您的一切需求并始终达到行业最高标准，VIP会员可享受独家奖金、受邀参加著名活动以及为您制定快速、有趣/高效的游戏体验，为您量身定制的多种多样的奖励机制。</p>
+            <p>Professional Customer Manager Team Ensures All Your Needs and Meets Industry's Highest Standards. VIP members can enjoy exclusive bonuses, invitations to renowned events, and a variety of customized rewards to provide you with fast, enjoyable, and efficient gaming experiences.</p>
             <dl>
-              <dt>VIP专属经理：</dt>
-              <dd>只关注您的需求</dd>
-              <dd>专为您量身定制的私人游戏体验</dd>
+              <dt>VIP Exclusive Manager：</dt>
+              <dd>Devoted to addressing your needs</dd>
+              <dd>Tailored private gaming experiences just for you</dd>
             </dl>
             <dl>
-              <dt>高额奖励：</dt>
-              <dd>专为您设计的优惠</dd>
-              <dd>免下注奖励</dd>
-              <dd>高快速的积分获取速度</dd>
+              <dt>High Rewards：</dt>
+              <dd>Exclusive offers designed for you</dd>
+              <dd>No-wager bonuses</dd>
+              <dd>Rapid accumulation of points</dd>
             </dl>
             <dl>
-              <dt>业内最高投注限额：</dt>
-              <dd>通过加密货币每日无限次的存款和取款</dd>
+              <dt>Industry's Highest Betting Limits：</dt>
+              <dd>Unlimited daily deposits and withdrawals with cryptocurrencies</dd>
             </dl>
             <dl>
-              <dt>VIP专属促销：</dt>
-              <dd>区别于普通会员的促销优惠</dd>
-              <dd>为您保留最好的游戏体验</dd>
-              <dd>仅VIP用户可见</dd>
-              <dd>个性化优惠</dd>
-              <dd>为您定制专属于您的娱乐服务</dd>
+              <dt>VIP Exclusive Promotions：</dt>
+              <dd>Distinguished from regular member promotions</dd>
+              <dd>Preserve the best gaming experience for you</dd>
+              <dd>Exclusive to VIP users</dd>
+              <dd>Personalized offers</dd>
+              <dd>Customized entertainment services exclusively for you</dd>
             </dl>
             <dl>
-              <dt>7*24小时优先服务：</dt>
-              <dd>无需排队，无需等待</dd>
-              <dd>我们首先关注您的需求</dd>
-              <dd>多种社交媒体联系方式，专属客户经理在线服务</dd>
-            </dl>
-          </div>
-        </div>
-        <div class="vc-rows">
-          <div class="vr-title">三、等级设置</div>
-          <div class="vr-conts">
-            <p>六种VIP等级，完整涵盖了您整个的游戏旅程</p>
-            <dl>
-              <dd>青铜：青铜1</dd>
-              <dd>白银：白银1-白银5</dd>
-              <dd>黄金：黄金1-黄金8</dd>
-              <dd>铂金：铂金1-铂金10</dd>
-              <dd>钛金：钛金1-钛金10</dd>
-              <dd>钻石：钻石1-钻石6</dd>
+              <dt>24/7 Priority Service：</dt>
+              <dd>No need to queue or wait</dd>
+              <dd>Your needs are our top priority</dd>
+              <dd>Multiple social media contact methods, dedicated customer managers providing online services.</dd>
             </dl>
           </div>
         </div>
         <div class="vc-rows">
-          <div class="vr-title">四、VIP俱乐部成员能够享受到的福利</div>
+          <div class="vr-title">3、Level Structure</div>
           <div class="vr-conts">
+            <p>Six VIP levels that comprehensively cover your entire gaming journey:</p>
             <dl>
-              <dd>助推奖金：您可以根据您每周、每月的游戏量获得奖金，玩的越多，获得的奖金越多</dd>
-              <dd>近期游戏表现奖金：运气不佳？Seabet会在您每次升级时根据您的损失提供额外资金</dd>
-              <dd>升级奖金：在每达到下个级别后获取奖金，所达到的级别越高，奖金越多</dd>
-              <dd>定制只属于您的优惠：对常规优惠不感兴趣？没关系，当您达到相应的VIP等级后，我们将会为您特别定制您所感兴趣的优惠内容，仅属于您个人的优惠内容</dd>
-              <dd>私人定制级娱乐体验：无论是各大赛事、演唱会、音乐会还是一场豪华旅行，当您成为我们的高级VIP会员，您的一切个人花销均由您所信赖的Seabet提供，您的行程由我们来安排</dd>
-              <dd>祝福礼金：重大的节日、您的生日、还有一些特殊的日子您都将获得一份祝福礼金，等级越高，礼金越高</dd>
-              <dd>专属VIP服务：为您分配一名支持并满足您的投注需求的专属VIP服务代表</dd>
-              <dd>更快速的积分获取：不同的VIP等级所获取的积分速度对比普通会员更快，更高的等级获取的积分也会更多，所能兑换的奖品也将更丰富</dd>
+              <dd>Bronze: Bronze 1</dd>
+              <dd>Silver: Silver 1 - Silver 5</dd>
+              <dd>Gold: Gold 1 - Gold 8</dd>
+              <dd>Platinum: Platinum 1 - Platinum 10</dd>
+              <dd>Titanium: Titanium 1 - Titanium 10</dd>
+              <dd>Diamond: Diamond 1 - Diamond 6</dd>
             </dl>
           </div>
         </div>
         <div class="vc-rows">
-          <div class="vr-title">五、VIP俱乐部升级条件</div>
+          <div class="vr-title">4、Benefits for VIP Club Members</div>
           <div class="vr-conts">
-            <p>Seabet贵宾会员俱乐部是以玩家投注额为基础，以每个自然月1日00:00开始计算到每月最后一天的23:59为计算周期，在计算周期内，玩家达到相应的投注金额即可晋级同时享受晋级后的等级待遇。</p>
-            <table>
-              <tr>
-                <th>VIP等级</th>
-                <th>所需投注额</th>
-                <th>升级奖金</th>
-              </tr>
-              <tr>
-                <td>青铜</td>
-                <td>100USDT</td>
-                <td>5</td>
-              </tr>
-              <tr>
-                <td>白银1</td>
-                <td>500USDT</td>
-                <td>10</td>
-              </tr>
-              <tr>
-                <td>白银2</td>
-                <td>1000USDT</td>
-                <td>15</td>
-              </tr>
-              <tr>
-                <td>白银3</td>
-                <td>3000USDT</td>
-                <td>25</td>
-              </tr>
-              <tr>
-                <td>白银4</td>
-                <td>6000USDT</td>
-                <td>30</td>
-              </tr>
-              <tr>
-                <td>白银5</td>
-                <td>1万USDT</td>
-                <td>50</td>
-              </tr>
-              <tr class="od">
-                <td>黄金1</td>
-                <td>1.5万USDT</td>
-                <td>70</td>
-              </tr>
-              <tr class="od">
-                <td>黄金2</td>
-                <td>2.5万USDT</td>
-                <td>80</td>
-              </tr>
-              <tr class="od">
-                <td>黄金3</td>
-                <td>4万USDT</td>
-                <td>100</td>
-              </tr>
-              <tr class="od">
-                <td>黄金4</td>
-                <td>6万USDT</td>
-                <td>120</td>
-              </tr>
-              <tr class="od">
-                <td>黄金5</td>
-                <td>8.5万USDT</td>
-                <td>150</td>
-              </tr>
-              <tr class="od">
-                <td>黄金6</td>
-                <td>11.5万USDT</td>
-                <td>170</td>
-              </tr>
-              <tr class="od">
-                <td>黄金7</td>
-                <td>15万USDT</td>
-                <td>200</td>
-              </tr>
-              <tr class="od">
-                <td>黄金8</td>
-                <td>19万USDT</td>
-                <td>230</td>
-              </tr>
-              <tr>
-                <td>铂金1</td>
-                <td>30万USDT</td>
-                <td>260</td>
-              </tr>
-              <tr>
-                <td>铂金2</td>
-                <td>42万USDT</td>
-                <td>300</td>
-              </tr>
-              <tr>
-                <td>铂金3</td>
-                <td>55万USDT</td>
-                <td>350</td>
-              </tr>
-              <tr>
-                <td>铂金4</td>
-                <td>69万USDT</td>
-                <td>400</td>
-              </tr>
-              <tr>
-                <td>铂金5</td>
-                <td>75万USDT</td>
-                <td>450</td>
-              </tr>
-              <tr>
-                <td>铂金6</td>
-                <td>90万USDT</td>
-                <td>500</td>
-              </tr>
-              <tr>
-                <td>铂金7</td>
-                <td>107万USDT</td>
-                <td>550</td>
-              </tr>
-              <tr>
-                <td>铂金8</td>
-                <td>125万USDT</td>
-                <td>600</td>
-              </tr>
-              <tr>
-                <td>铂金9</td>
-                <td>145万USDT</td>
-                <td>660</td>
-              </tr>
-              <tr>
-                <td>铂金10</td>
-                <td>160万USDT</td>
-                <td>700</td>
-              </tr>
-              <tr class="od">
-                <td>钛金1</td>
-                <td>190万USDT</td>
-                <td>800</td>
-              </tr>
-              <tr class="od">
-                <td>钛金2</td>
-                <td>225万USDT</td>
-                <td>900</td>
-              </tr>
-              <tr class="od">
-                <td>钛金3</td>
-                <td>257万USDT</td>
-                <td>1000</td>
-              </tr>
-              <tr class="od">
-                <td>钛金4</td>
-                <td>290万USDT</td>
-                <td>1100</td>
-              </tr>
-              <tr class="od">
-                <td>钛金5</td>
-                <td>325万USDT</td>
-                <td>1200</td>
-              </tr>
-              <tr class="od">
-                <td>钛金6</td>
-                <td>359万USDT</td>
-                <td>1300</td>
-              </tr>
-              <tr class="od">
-                <td>钛金7</td>
-                <td>395万USDT</td>
-                <td>1380</td>
-              </tr>
-              <tr class="od">
-                <td>钛金8</td>
-                <td>432万USDT</td>
-                <td>1500</td>
-              </tr>
-              <tr class="od">
-                <td>钛金9</td>
-                <td>470万USDT</td>
-                <td>1650</td>
-              </tr>
-              <tr class="od">
-                <td>钛金10</td>
-                <td>509万USDT</td>
-                <td>1700</td>
-              </tr>
-              <tr>
-                <td>钻石1</td>
-                <td>600万USDT</td>
-                <td>2000</td>
-              </tr>
-              <tr>
-                <td>钻石2</td>
-                <td>1000万USDT</td>
-                <td>5000</td>
-              </tr>
-              <tr>
-                <td>钻石3</td>
-                <td>2000万USDT</td>
-                <td>10000</td>
-              </tr>
-              <tr>
-                <td>钻石4</td>
-                <td>3500万USDT</td>
-                <td>15000</td>
-              </tr>
-              <tr>
-                <td>钻石5</td>
-                <td>5500万USDT</td>
-                <td>20000</td>
-              </tr>
-              <tr>
-                <td>钻石6</td>
-                <td>8000万USDT</td>
-                <td>35000</td>
-              </tr>
-            </table>
-            <p>*VIP等级会于每月初始之日清零</p>
-            <p>*VIP积分永久保留</p>
+            <dl>
+              <dd>Boost Bonuses: You can earn bonuses based on your weekly and monthly gaming volume. The more you play, the more bonuses you receive.</dd>
+              <dd>Recent Game Performance Bonus: Having a streak of bad luck? Seabet will provide additional funds based on your losses every time you level up.</dd>
+              <dd>Level Up Bonuses: Receive bonuses upon reaching the next level. The higher the level you achieve, the greater the bonus.</dd>
+              <dd>Customized Exclusive Offers: Not interested in regular promotions? No problem. Once you reach the corresponding VIP level, we will specially tailor offers based on your interests, exclusively for you</dd>
+              <dd>Personalized Entertainment Experience: Whether it's major sports events, concerts, music shows, or a luxurious trip, as a senior VIP member, all your personal expenses will be covered by the trusted Seabet, and we will arrange your itinerary.</dd>
+              <dd>Exclusive VIP Service: You will be assigned a dedicated VIP service representative who will support and meet your betting needs</dd>
+              <dd>Faster Points Accumulation: VIP levels earn points at a faster rate compared to regular members. Higher levels earn more points, leading to a wider range of prizes for redemption.</dd>
+            </dl>
           </div>
         </div>
         <div class="vc-rows">
-          <div class="vr-title">六、VIP俱乐部福利</div>
+          <div class="vr-title">5、How to Become a VIP Member</div>
           <div class="vr-conts">
-            <table>
-              <tr>
-                <th>VIP级别</th>
-                <th>积分系数</th>
-                <th>专属优惠</th>
-                <th>定制优惠</th>
-              </tr>
-              <tr>
-                <td>青铜</td>
-                <td>无</td>
-                <td>无</td>
-                <td>无</td>
-              </tr>
-              <tr>
-                <td>白银</td>
-                <td>1.1倍</td>
-                <td>无</td>
-                <td>无</td>
-              </tr>
-              <tr>
-                <td>黄金</td>
-                <td>1.2倍</td>
-                <td>无</td>
-                <td>无</td>
-              </tr>
-              <tr>
-                <td>铂金</td>
-                <td>1.3倍</td>
-                <td>√</td>
-                <td>无</td>
-              </tr>
-              <tr>
-                <td>钛金</td>
-                <td>1.5倍</td>
-                <td>√</td>
-                <td>√</td>
-              </tr>
-              <tr>
-                <td>钻石</td>
-                <td>2倍</td>
-                <td>√</td>
-                <td>√</td>
-              </tr>
-            </table>
-            <p class="mark">
-              <i class="iconfont icon-notice"></i>
-              积分系数说明：积分系数决定玩家获取积分的速度与倍数，Seabet积分获取公式为玩家在任意场馆的投注金额*0.1*对应级别的积分系数，如玩家等级为铂金，投注金额为5000，那么根据积分计算公式，该玩家所获积分为5000*0.1*1.3=650积分。Seabet积分用途广泛，玩家可通过积分在积分商城中兑换等值礼品及服务项目。
-            </p>
-          </div>
-        </div>
-        <div class="vc-rows">
-          <div class="vr-title">七、如何成为我们的VIP会员</div>
-          <div class="vr-conts">
-            <p>如何成为Seabet的VIP会所会员</p>
+            <p>How to become a VIP member of Seabet's VIP Club:</p>
             <dl>
               <dd>
-                继续投注：成为 VIP 的关键是始终投注各种体育赛事和赌场游戏。Seabet 将奖励您的一致性。作为顶级加密博彩网站，请定期使用您的数字资产对 Seabet 提供的任何赛事和赌场游戏进行投注。也可以尝试
-                USDT 投注，并持续使用您的资金。
+                Keep Betting: The key to becoming a VIP is consistently placing bets on various sports events and casino games. Seabet rewards your consistency. As a top cryptocurrency gambling site, regularly use your digital assets to place bets on any sports events and casino games offered by Seabet. You can also try betting with USDT and continue using your funds
               </dd>
               <dd>
-                参加定期活动：Seabet 的促销活动包括您可以随时参加的各种活动和竞赛。定期参加其中任何一个活动都会让您更有机会获得 VIP 会所邀请。不仅如此，您还可以在 Seabet 的其他俱乐部功能中更加活跃
+                Participate in Regular Events: Seabet's promotional activities include various events and competitions that you can participate in at any time. Regular participation in any of these events will increase your chances of receiving an invitation to the VIP Club. Moreover, you can be more active in other club features offered by Seabet.
               </dd>
-              <dd>等待邀请：您需要做的最后一件事就是等待我们向您发送邀请。继续玩游戏并参加豪客赛。经过几次赌场博彩和体育博彩后，一旦您被认为值得成为 VIP 俱乐部会员，您将立即获得邀请。</dd>
               <dd>
-                作为Seabet会所VIP体验高尚生活：将您的 USDT 博彩体验带入一个全新的世界，同时享受作为 Seabet
-                贵宾俱乐部会员的顶级福利。当您进行体育博彩和赌场游戏时，作为有价值的玩家享受最好的乐趣、快速和公平的投注。
+                Await the Invitation: The final thing you need to do is wait for us to send you an invitation. Continue playing games and participating in high roller events. After a few rounds of casino gambling and sports betting, once you are deemed worthy of becoming a VIP Club member, you will receive an invitation immediately.
+              </dd>
+              <dd>
+                Experience the Luxury Life as a Seabet VIP: Take your USDT gambling experience to a whole new level while enjoying top-notch privileges as a Seabet VIP Club member. As a valued player, you'll experience the best fun, fast, and fair betting when engaging in sports betting and casino games.
               </dd>
             </dl>
           </div>
