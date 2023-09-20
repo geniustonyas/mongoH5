f0d0b839659e40c12dac8d57984a70d82975af28
Your Name <you@example.com>
2023-09-20 14:30:57 +0800

    a

--- src/components/layout/IndexHeader.vue
+++ src/components/layout/IndexHeader.vue
@@ -1,33 +1,32 @@
 <template>
   <header class="header">
-    <nav :class="userStore.userInfo.id == '' ? 'head-menu' : 'head-menu top-head'">
-      <!-- logo 跳转主页 -->
-      <div class="hm-l">
-        <a @click="appStore.showSideBar = !appStore.showSideBar" class="icon-btn navbar-left">
-          <i :class="appStore.showSideBar ? 'iconfont icon-close' : 'iconfont icon-zhedie1'" />
-        </a>
-        <!-- <a v-if="userStore.userInfo.id == ''" class="icon-btn" @click="router.push({ name: 'mall' })">
-          <i class="iconfont icon-shangcheng" />
-        </a> -->
-      </div>
+    <!--<nav :class="userStore.userInfo.id == '' ? 'head-menu' : 'head-menu top-head'">-->
+    <!-- logo 跳转主页 -->
+    <!--<div class="hm-l">
+      <a @click="appStore.showSideBar = !appStore.showSideBar" class="icon-btn navbar-left">
+        <i :class="appStore.showSideBar ? 'iconfont icon-close' : 'iconfont icon-zhedie1'" />
+      </a>-->
+    <!-- <a v-if="userStore.userInfo.id == ''" class="icon-btn" @click="router.push({ name: 'mall' })">
+      <i class="iconfont icon-shangcheng" />
+    </a> -->
+    <!--</div>-->
+    <!-- 用户信息 -->
+    <!--<div class="hm-m">
+      <template v-if="userStore.userInfo.id">
+        <div class="user-info" @click="router.push({ name: 'fund' })">
+          <b>{{ userStore.userInfo.userName }}</b>
+          <a>
+            <label>+</label>
+            <span>{{ Math.floor(parseFloat(userStore.userInfo?.balance) * 1000) / 1000 }}</span>
+            {{ userStore.userInfo?.defaultCurrencyCode }}-->
+    <!-- {{ userStore.userInfo?.defaultCurrencyCode == 'BTC' ? userStore.userInfo?.btcUnit?.currencyUnit : userStore.userInfo?.defaultCurrencyCode }} -->
+    <!--</a>
+        </div>
+      </template>
+    </div>-->
+    <!-- 积分商城或登录注册 -->
+    <!--<div class="hm-r">
 
-      <!-- 用户信息 -->
-      <div class="hm-m">
-        <template v-if="userStore.userInfo.id">
-          <div class="user-info" @click="router.push({ name: 'fund' })">
-            <b>{{ userStore.userInfo.userName }}</b>
-            <a>
-              <label>+</label>
-              <span>{{ Math.floor(parseFloat(userStore.userInfo?.balance) * 1000) / 1000 }}</span>
-              {{ userStore.userInfo?.defaultCurrencyCode }}
-              <!-- {{ userStore.userInfo?.defaultCurrencyCode == 'BTC' ? userStore.userInfo?.btcUnit?.currencyUnit : userStore.userInfo?.defaultCurrencyCode }} -->
-            </a>
-          </div>
-        </template>
-      </div>
-
-      <!-- 积分商城或登录注册 -->
-      <div class="hm-r">
         <template v-if="userStore.userInfo.id">
           <a class="icon-btn" @click="router.push({ name: 'clubHouse' })">
             <img :src="getAssetsFile(`grade/${userStore.userInfo.vipCode}.png`)" />
@@ -37,29 +36,70 @@
           </a>
           <a class="icon-btn" @click="loginTo('message')">
             <i class="iconfont icon-message" />
-            <em class="badge" />
-            <!-- <em v-if="userStore.newMessageCount > 0" class="badge" /> -->
-          </a>
-        </template>
-        <template v-else>
-          <a class="btn" @click="router.push({ name: 'reg' })">{{ t('reg') }}</a>
-          <a class="btn btn-primary" @click="router.push({ name: 'login' })">{{ t('login') }}</a>
-        </template>
+            <em class="badge" />-->
+    <!-- <em v-if="userStore.newMessageCount > 0" class="badge" /> -->
+    <!--</a>
+          </template>
+          <template v-else>
+            <a class="btn" @click="router.push({ name: 'reg' })">{{ t('reg') }}</a>
+            <a class="btn btn-primary" @click="router.push({ name: 'login' })">{{ t('login') }}</a>
+          </template>
+        </div>
+    </nav>-->
+
+
+    <nav class="head-menu index-head">
+      <div class="ih-l">
+        <a @click="appStore.showSideBar = !appStore.showSideBar" class="icon-btn navbar-left">
+          <i :class="appStore.showSideBar ? 'iconfont icon-close' : 'iconfont icon-zhedie1'" />
+        </a>
+      </div>
+      <div class="ih-r">
+        <div class="ihr-bd">
+          <div class="rb-l">
+            <b>{{ userStore.userInfo.userName }}</b>
+            <a>
+              <label>+</label>
+              <span>{{ Math.floor(parseFloat(userStore.userInfo?.balance) * 1000) / 1000 }}</span>
+              {{ userStore.userInfo?.defaultCurrencyCode == 'BTC' ? userStore.userInfo?.btcUnit?.currencyUnit : userStore.userInfo?.defaultCurrencyCode }}
+            </a>
+          </div>
+          <div class="rb-r">
+            <a class="icon-btn" @click="router.push({ name: 'clubHouse' })">
+              <img :src="getAssetsFile(`grade/${userStore.userInfo.vipCode}.png`)" />
+            </a>
+            <a class="icon-btn" @click="router.push({ name: 'mall' })">
+              <i class="iconfont icon-shangcheng" />
+            </a>
+            <a class="icon-btn" @click="loginTo('message')">
+              <i class="iconfont icon-message" />
+              <em class="badge" />
+            </a>
+          </div>
+        </div>
       </div>
     </nav>
+
   </header>
+
+
+
+
+
+
+
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { useI18n } from 'vue-i18n'
+  import { useRouter } from 'vue-router'
+  import { useI18n } from 'vue-i18n'
 
-import { useAppStore } from '@/store/modules/app'
-import { useUserStore } from '@/store/modules/user'
-import { getAssetsFile, loginTo } from '@/utils'
+  import { useAppStore } from '@/store/modules/app'
+  import { useUserStore } from '@/store/modules/user'
+  import { getAssetsFile, loginTo } from '@/utils'
 
-const appStore = useAppStore()
-const userStore = useUserStore()
-const router = useRouter()
-const { t } = useI18n()
+  const appStore = useAppStore()
+  const userStore = useUserStore()
+  const router = useRouter()
+  const { t } = useI18n()
 </script>
