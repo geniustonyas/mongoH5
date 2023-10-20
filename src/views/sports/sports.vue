4b729873f8bf91832775a7e0899cb95db83488fc
Your Name <you@example.com>
2023-10-20 16:14:38 +0800

    a

--- src/views/sports/sports.vue
+++ src/views/sports/sports.vue
@@ -5,7 +5,7 @@
       <IndexTab tab="sports" />
 
       <!-- 体育 -->
-      <div class="sports-box">
+      <!--<div class="sports-box">
         <div class="sb-a">
           <div class="item">
             <div class="i-l" v-lazy:background-image="getAssetsFile('svg/seabetSport.svg')" />
@@ -28,6 +28,53 @@
             </div>
           </div>
         </div>
+      </div>-->
+
+      <div class="sports-box">
+        <div class="sb-items">
+
+          <div class="item seabet">
+            <div class="is-a"><span>Seabet</span>Sports</div>
+            <div class="is-b">{{ t('btiSportIntro') }}</div>
+            <ul class="is-c">
+              <li><img :src="getAssetsFile('sports/seabet_icon_1.svg')" />xxxxxx</li>
+              <li><img :src="getAssetsFile('sports/seabet_icon_2.svg')" />xxxxxx</li>
+              <li><img :src="getAssetsFile('sports/seabet_icon_3.svg')" />xxxxxx</li>
+              <li><img :src="getAssetsFile('sports/seabet_icon_4.svg')" />xxxxxx</li>
+              <li><img :src="getAssetsFile('sports/seabet_icon_5.svg')" />xxxxxx</li>
+            </ul>
+            <div class="is-d">
+              <a>PLAY NOW</a>
+            </div>
+          </div>
+          <div class="item saba">
+            <div class="is-a"><span>Seabet</span>Sports</div>
+            <div class="is-b">{{ t('sabaSportIntro') }}</div>
+            <ul class="is-c">
+              <li><img :src="getAssetsFile('sports/saba_icon_1.svg')" />xxxxxx</li>
+              <li><img :src="getAssetsFile('sports/saba_icon_2.svg')" />xxxxxx</li>
+              <li><img :src="getAssetsFile('sports/saba_icon_3.svg')" />xxxxxx</li>
+              <li><img :src="getAssetsFile('sports/saba_icon_4.svg')" />xxxxxx</li>
+              <li><img :src="getAssetsFile('sports/saba_icon_5.svg')" />xxxxxx</li>
+            </ul>
+            <div class="is-d">
+              <a>PLAY NOW</a>
+            </div>
+          </div>
+
+        </div>
+        <!--<ul class="sb-items">
+          <li class="i-seabet">
+            <div class="is-a"><span>Seabet</span>Sports</div>
+            <div class="is-b">{{ t('btiSportIntro') }}</div>
+            <div class="is-c">
+
+            </div>
+          </li>
+          <li class="i-saba">
+            b
+          </li>
+        </ul>-->
       </div>
 
       <IndexFooter :currency-code="currencyCode" :exchange-rate="exchangeRate" />
@@ -40,24 +87,24 @@
 </template>
 
 <script setup lang="ts">
-import { useI18n } from 'vue-i18n'
+  import { useI18n } from 'vue-i18n'
 
-import IndexHeader from '@/components/layout/IndexHeader.vue'
-import IndexFooter from '@/components/layout/IndexFooter.vue'
-import IndexTab from '@/components/layout/IndexTab.vue'
-import Footer from '@/components/layout/Footer.vue'
-import Sidebar from '@/components/layout/SideBar.vue'
+  import IndexHeader from '@/components/layout/IndexHeader.vue'
+  import IndexFooter from '@/components/layout/IndexFooter.vue'
+  import IndexTab from '@/components/layout/IndexTab.vue'
+  import Footer from '@/components/layout/Footer.vue'
+  import Sidebar from '@/components/layout/SideBar.vue'
 
-import { startGame } from '@/composables/startGame'
+  import { startGame } from '@/composables/startGame'
 
-import { useAppStore } from '@/store/modules/app'
-import { getAssetsFile } from '@/utils'
-import { getExchangeRate } from '@/composables/getExchangeRate'
-import { GameType } from '@/utils/constant'
+  import { useAppStore } from '@/store/modules/app'
+  import { getAssetsFile } from '@/utils'
+  import { getExchangeRate } from '@/composables/getExchangeRate'
+  import { GameType } from '@/utils/constant'
 
-const appStore = useAppStore()
-const { t } = useI18n()
+  const appStore = useAppStore()
+  const { t } = useI18n()
 
-// 汇率相关
-const { currencyCode, exchangeRate } = getExchangeRate()
+  // 汇率相关
+  const { currencyCode, exchangeRate } = getExchangeRate()
 </script>
